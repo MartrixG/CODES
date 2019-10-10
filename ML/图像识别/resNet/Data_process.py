c4818e5c4e50162a2d8c4ca0d9398cdb8c7cdc57
Martrix <44332912+1171000405@users.noreply.github.com>
2019-10-11 00:58:28 +0800

    data process

---
+++ ML/图像识别/resNet/Data_process.py
@@ -0,0 +1,61 @@
+#Python 3.6.9 64-bit(tensorflow-gpu)
+
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
+from keras.datasets import cifar10
+from keras.utils import np_utils
+import keras.backend as K
+import numpy as np
+import random
+
+K.set_image_data_format('channels_first')
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+def load_data(data_set = "cifar10", num_dev = 500):
+    """
+    get the chosen data set
+
+    Arguments:
+    data_set: the name of data set, default is cifar10
+    num_dev: the size(number of data) of dev set, default is 500
+
+    returns: x_tr, y_tr, x_te, y_te, x_de, y_de
+    """
+    if data_set == "cifar10":
+        x_tr, y_tr = load_cifar10_train()
+        x_te, y_te = load_cifar10_test()
+        x_de, y_de = load_dev(num_dev, x_tr, y_tr)
+        return x_tr, y_tr, x_te, y_te, x_de, y_de
+    else:
+        pass
+
+def load_cifar10_train():
+    x_tr = x_train.astype('float32') / 255
+    y_tr = np_utils.to_categorical(y_train, 10)
+    return x_tr, y_tr
+
+def load_cifar10_test():
+    x_te = x_test.astype('float32') / 255
+    y_te = np_utils.to_categorical(y_test, 10)
+    return x_te, y_te
+
+def load_dev(num_dev, x_tr, y_tr):
+    """
+    According to the param get the dev set.
+
+    Arguments:
+    num_dev --- the size of dev set
+    x_tr --- train data of x
+    y_tr --- train data of y
+
+    returns: x_dev, y_dev
+    """
+    x_dev = []
+    y_dev = []
+    for i in range(num_dev):
+        num = random.randint(0, x_tr.shape[0])
+        x_dev.append(x_tr[num])
+        y_dev.append(y_tr[num])
+    x_dev = np.array(x_dev)
+    y_dev = np.array(y_dev)
+    return x_dev, y_dev
--- ML/图像识别/resNet/ResNet.py
+++ ML/图像识别/resNet/ResNet.py
@@ -3,30 +3,20 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
 
-from keras.utils import np_utils
 from keras.layers import Input, Dense, Flatten, AveragePooling2D, Add, Activation, Conv2D
 from keras.models import Model, load_model
-from keras.initializers import glorot_uniform
-from keras.datasets import cifar10
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau
 from keras.regularizers import l2
-import keras.backend as K
-import Res_blocks as block
 
-K.set_image_data_format('channels_first')#the first dim is channel
-K.set_learning_phase(1)
+import Res_blocks as block
+import Data_process as data
 
 num_classes = 10
 batch_size = 32
-epochs = 200
+epochs = 100
 
 #load train data and labels
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
-y_train = np_utils.to_categorical(y_train, num_classes)
-y_test = np_utils.to_categorical(y_test, num_classes)
+x_train, y_train, x_test, y_test, x_dev, y_dev = data.load_data()
 
 #model = ResNet50(input_shape = (3, 32, 32), classes = 10)
 def ResNet50(input_shape = (3, 32, 32), classes = 10):
@@ -75,7 +65,7 @@ def ResNet50(input_shape = (3, 32, 32), classes = 10):
     X = AveragePooling2D(pool_size = (2, 2), padding = "same")(X)#64, 2, 2
 
     X = Flatten()(X)
-    X = Dense(classes, activation = "softmax", name = "fc" + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Dense(classes, activation = "softmax", name = "fc" + str(classes), kernel_initializer = 'he_normal')(X)
 
     model = Model(inputs = X_input, outputs = X, name = "ResNet50")
 
@@ -137,7 +127,9 @@ def lr_sch(epoch):
 lr_scheduler = LearningRateScheduler(lr_sch)
 lr_reducer = ReduceLROnPlateau(monitor = 'val_acc', factor = 0.2, patience = 5, mode = 'max', min_lr = 1e-3)
 callbacks = [checkpoint, lr_scheduler, lr_reducer]
-model.fit(x_train, y_train, epochs = epochs, batch_size = batch_size, validation_data = (x_test,y_test), verbose = 1,callbacks = callbacks)
+
+model.fit(x_dev, y_dev, epochs = epochs, batch_size = batch_size)
+#model.fit(x_dev, y_dev, epochs = epochs, batch_size = batch_size, validation_data = (x_test,y_test), verbose = 1,callbacks = callbacks)
 #model.summary()
 preds = model.evaluate(x_test, y_test)
 print ("Loss = " + str(preds[0]))
