42ec3f842904b565e55382f0d449568358fb7f72
Martrix <44332912+1171000405@users.noreply.github.com>
2020-04-05 11:52:48 +0800

    编译原理数据

---
+++ 编译原理/data/test_code/code.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int abc=1.2;
+    float c=1e22, __oi = 023, a1b2_ = 0x8A2E,a=1,b=23;
+    const double qwe=23-0x2+07
+    while(a<12)
+    {
+        a+=1;
+    } 
+    switch (b)
+    {
+    case 23:
+        break;
+    default:
+        break;
+    }
+}
