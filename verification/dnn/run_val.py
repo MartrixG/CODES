328e645cd3bb2f06fb4b34890c3274c2642e363e
Martrix <44332912+1171000405@users.noreply.github.com>
2020-06-15 10:23:58 +0800

    iterate seed

--- verification/dnn/run_val.py
+++ verification/dnn/run_val.py
@@ -22,13 +22,21 @@ def count_p(p_list):
 def get_process(opt, dataset, m, n):
     process_list = []
     all_arg = []
-    for key in opt:
-        for j in opt[key]:
-            arg = {key: j, 'c_in': m, 'c_out': n, 'dataset': dataset}
-            if arg.__contains__('first_neurons') is False:
-                arg['first_neurons'] = m // 2
-            process_list.append(Process(target=main, args=(arg,)))
-            all_arg.append(arg)
+    seed = opt['seed']
+    opt.pop('seed')
+    for s in seed:
+        for key in opt:
+            for j in opt[key]:
+                arg = {'seed': s, 'c_in': m, 'c_out': n, 'dataset': dataset}
+                if key == 'cross':
+                    arg['cross_link'] = j[0]
+                    arg['fully_cross'] = j[1]
+                else:
+                    arg[key] = j
+                if arg.__contains__('first_neurons') is False:
+                    arg['first_neurons'] = m // 2
+                process_list.append(Process(target=main, args=(arg,)))
+                all_arg.append(arg)
     return process_list, all_arg
 
 
@@ -63,16 +71,15 @@ def all_train(dataset):
     first_neurons = [2 * m, 4 * m, m // 2, m // 4]
     change = [2, 4, 1 / 2, 1 / 4]
     activate_func = ['tanh', 'sigmoid']
-    opt = {'hidden_layers': hidden_layers, 'first_neurons': first_neurons, 'change': change, 'activate_func': activate_func}
+    cross = [[True, False], [True, True]]
+    seed = [0, 1]
+    opt = {'hidden_layers': hidden_layers,
+           'first_neurons': first_neurons,
+           'change': change,
+           'activate_func': activate_func,
+           'cross': cross,
+           'seed': seed}
     process_list, all_arg = get_process(opt, dataset, m, n)
-
-    arg = {'c_in': m, 'c_out': n, 'dataset': 'HAPT', 'first_neurons': m, 'cross_link': True}
-    process_list.append(Process(target=main, args=(arg,)))
-    all_arg.append(arg)
-
-    arg = {'c_in': m, 'c_out': n, 'dataset': 'HAPT', 'first_neurons': m, 'cross_link': True, 'fully_cross':True}
-    process_list.append(Process(target=main, args=(arg,)))
-    all_arg.append(arg)
 
     return process_list, all_arg
 
@@ -85,31 +92,35 @@ def first_neurons_train(dataset):
          m // 4,
          m // 2,
          m * 2]
-    opt = {'first_neurons': first_neurons}
+    seed = [0, 1]
+    opt = {'first_neurons': first_neurons, 'seed': seed}
     return get_process(opt, dataset, m, n)
 
 
 def cross_hidden_train(dataset):
     m, n = get_mn(dataset)
     hidden_layers = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-    opt = {'hidden_layers': hidden_layers}
+    seed = [0, 1, 2, 3, 4]
+    opt = {'hidden_layers': hidden_layers, 'seed': seed}
     return get_process(opt, dataset, m, n)
 
 
 def hidden_train(dataset):
     m, n = get_mn(dataset)
     hidden_layers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    opt = {'hidden_layers': hidden_layers}
+    seed = [0, 1]
+    opt = {'hidden_layers': hidden_layers, 'seed': seed}
     return get_process(opt, dataset, m, n)
 
 
 def cross_seed(dataset):
     m, n = get_mn(dataset)
     seed = [0, 1, 2, 3, 4]
-    opt = {'seed': seed}
+    cross = [[True, False], [True, True]]
+    opt = {'seed': seed, 'cross': cross}
     return get_process(opt, dataset, m, n)
 
 
 if __name__ == '__main__':
-    process, args = cross_hidden_train('HAPT')
+    process, args = cross_hidden_train('UJI')
     run_process(process, 5)
